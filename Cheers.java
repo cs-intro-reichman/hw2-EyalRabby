4ef4d1bc1bbff26c1179a919fefd64b6404535d4
EyalRabby <eyal.rabby@post.runi.ac.il>
2024-11-21 12:49:57 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -4,10 +4,30 @@ public class Cheers {
                 String word = args[0];
                 int cheersNum = Integer.parseInt(args[1]);
 
-                String WORD = word.toUpperCase();
+                String upperCaseString = ToUpperCase(word);
+                CheerGenerator(upperCaseString, cheersNum);
 
-                for (int i = 0; i < WORD.length(); i++) {
-                        char L = WORD.charAt(i);
+        }
+        public static String ToUpperCase(String word) {
+                String upperCaseString = "";
+
+                for (int i = 0; i < word.length(); i++){
+                        int C = word.charAt(i);
+
+                    if ( C >= 97 && C <= 122) {
+                        upperCaseString += (char)(C - 32);
+                    } else {
+                        upperCaseString += (char)C; 
+                    }
+                }
+                return upperCaseString;
+
+        }
+               
+        public static void CheerGenerator(String upperCaseString, int cheersNum) {
+
+                for (int i = 0; i < upperCaseString.length(); i++) {
+                        char L = upperCaseString.charAt(i);
                         if (L == 'A' || L == 'E' || L == 'F' || L == 'H' || L == 'I' || L == 'L' || L == 'M' || L == 'N' || L == 'O' || L == 'R' || L == 'S' || L == 'X') {
                                 System.out.println("Give me an " + L + ": " + L + "!");
                         } else {
@@ -18,7 +38,7 @@ public class Cheers {
                 System.out.println("What does that spell?");
 
                 for (int i = 0; i < cheersNum; i++) {
-                        System.out.println(WORD + "!!!");
+                        System.out.println(upperCaseString + "!!!");
                 }
 
         }
